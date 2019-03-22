2e62d749e07a5e3d42cf0504e500687240d882e0
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-03-23 02:31:28 +0530

    Add basic slider

--- image_slider/main.js
+++ image_slider/main.js
@@ -1,17 +1,9 @@
 let sliderImages = ['img_1','img_2','img_3','img_4','img_5'];
-const sliderCotainer = document.getElementById('slider');
 const imageElement = document.getElementById('image');
-const prevSlideLink =document.getElementById('prev');
-const nextSlideLink = document.getElementById('next');
+const prevSlideButton =document.getElementById('prev');
+const nextSlideButton = document.getElementById('next');
 let currentIndex = 0;
-let intervalID = '';
-let timeOutID = '';
-
-//apply filter function
-
-//change slide function
-
-
+  
 const nextSlideIndex = () => {
     if(currentIndex < (sliderImages.length-1)){
         currentIndex++; 
@@ -31,44 +23,21 @@ const prevSlideIndex = () =>{
 }
 
 const changeSlide = () => {
-    imageElement.setAttribute('src',`images/${sliderImages[currentIndex]}.jpg`);
-    console.log('hello I am a slide' + currentIndex);
+timerID = setTimeout(()=>{
+        imageElement.setAttribute('src',`images/${sliderImages[currentIndex]}.jpg`);
+},300);
 }
-
  
-const autoRunSlider = ()=>{
-     intervalID = setInterval(()=>{
-         nextSlideIndex();
-         changeSlide();
-     },4000);
-    console.log(currentIndex);
-}
-
-//pause slider on hover
-//imageElement.addEventListener('mouseover',()=> clearInterval(intervalID));
-//imageElement.addEventListener('mouseleave',()=> setInterval(changeSlide,4000));
-
-//next slide
-nextSlideLink.addEventListener('click',(event)=>{
-    event.preventDefault();
-    clearInterval(intervalID);
+//next slide button
+nextSlideButton.addEventListener('click',(event)=>{
     nextSlideIndex();
     changeSlide();
-   let timeOutID = setTimeout(autoRunSlider,1000);
 })
 
-//prev slide
-prevSlideLink.addEventListener('click',(event)=>{
-    event.preventDefault();
-    clearInterval(intervalID)
+//prev slide button
+prevSlideButton.addEventListener('click',(event)=>{
     prevSlideIndex();
     changeSlide();
-   let timeOutID = setTimeout(autoRunSlider,1000);
 })
+clearTimeout(timerID);
 
-//let intervalID = setInterval(changeSlide,4000);
-//clearInterval(intervalID);
-
-//jump to slide bullets
-clearTimeout(timeOutID);
-autoRunSlider();
