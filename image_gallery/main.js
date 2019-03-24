96b315b9bb7a41bc0d3409eefbf61aa8e1fcd57d
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-03-25 01:45:34 +0530

    Add fade animation effect to image

--- image_gallery/main.js
+++ image_gallery/main.js
@@ -3,6 +3,9 @@ const imagePannels = document.getElementById('img-pannels');
 
 imagePannels.addEventListener('click',(event)=>{
     let imagePath = event.target.getAttribute('src');
-    mainImage.setAttribute('src',imagePath);
-
+    mainImage.classList.add('fade-out');     
+    setTimeout(()=>{
+        mainImage.setAttribute('src',imagePath);
+        mainImage.classList.remove('fade-out');
+    },1300)    
 });
